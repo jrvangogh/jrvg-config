3283cbf2f226d3d50d826aa8d2503925dd46594d
Jacob van Gogh <jacob.vangogh@omni.sc>
2019-05-30 19:20:13 -0700

    Added matplotlibrc

--- symlink_config_files.sh
+++ symlink_config_files.sh
@@ -5,3 +5,4 @@ ln -s ./vimrc ~/.vimrc
 ln -s ./sshconfig ~/.ssh/config
 ln -s ./gitconfig ~/.gitconfig
 ln -s ./gitconfig_work_include ~/OmniSpace/.gitconfig_work_include
+ln -s ./matplotlibrc ~/.matplotlib/matplotlibrc

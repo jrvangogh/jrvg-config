7c8fa798efa705fec95e3f7b3dc275e59aa1acb3
Jacob van Gogh <jacob.vangogh@omni.sc>
2019-05-30 19:13:50 -0700

    Renamed files for clarity and added script to symlink everything

---
+++ link_config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ln -s ./bash_profile ~/.bash_profile
+ln -s ./vimrc ~/.vimrc
+ln -s ./sshconfig ~/.ssh/config
+ln -s ./gitconfig ~/.gitconfig
+ln -s ./gitconfig_work_include ~/OmniSpace/.gitconfig_work_include

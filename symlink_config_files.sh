49043fffbc1e71d704c5755996aef8295ef533c6
Jacob Ryan van Gogh <jrvangogh@gmail.com>
2019-09-11 13:18:48 -0700

    Now work at Lyft

--- symlink_config_files.sh
+++ symlink_config_files.sh
@@ -2,11 +2,11 @@
 
 ln -s "$(pwd)"/bash_profile ~/.bash_profile
 ln -s "$(pwd)"/vimrc ~/.vimrc
-ln -s "$(pwd)"/sshconfig ~/.ssh/config
+# ln -s "$(pwd)"/sshconfig ~/.ssh/config (Use Lyft's)
 ln -s "$(pwd)"/gitconfig ~/.gitconfig
-ln -s "$(pwd)"/gitconfig_work_include ~/OmniSpace/.gitconfig_work_include
+ln -s "$(pwd)"/gitconfig_work_include ~/LyftSpace/.gitconfig_work_include
 ln -s "$(pwd)"/matplotlibrc ~/.matplotlib/matplotlibrc
-ln -s "$(pwd)"/awsconfig ~/.aws/config
+# ln -s "$(pwd)"/awsconfig ~/.aws/config (Use Lyft's)
 ln -s "$(pwd)"/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
 ln -s "$(pwd)"/ideavimrc ~/.ideavimrc
 

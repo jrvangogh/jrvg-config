6f40aa8e15a14efc2160c3589dd6895e4e44b691
Jacob Ryan van Gogh <jrvangogh@gmail.com>
2024-07-25 08:36:30 -0700

    updates

--- symlink_config_files.zsh
+++ symlink_config_files.zsh
@@ -2,18 +2,14 @@
 emulate -LR zsh
 
 ln -s "$(pwd)"/zshrc ~/.zshrc
-ln -s "$(pwd)"/zprofile ~/.zprofile
 ln -s "$(pwd)"/vimrc ~/.vimrc
-# ln -s "$(pwd)"/sshconfig ~/.ssh/config (use Adept's)
+ln -s "$(pwd)"/sshconfig ~/.ssh/config
 ln -s "$(pwd)"/global_gitignore ~/.global_gitignore
 ln -s "$(pwd)"/gitconfig ~/.gitconfig
-ln -s "$(pwd)"/gitconfig_work_include ~/src/.gitconfig_work_include
 mkdir -p ~/.matplotlib
 ln -s "$(pwd)"/matplotlibrc ~/.matplotlib/matplotlibrc
-# ln -s "$(pwd)"/awsconfig ~/.aws/config (not needed)
 mkdir -p ~/.jupyter
 ln -s "$(pwd)"/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
 ln -s "$(pwd)"/ideavimrc ~/.ideavimrc
 ln -s "$(pwd)"/vsc_settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s "$(pwd)"/vsc_keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-
